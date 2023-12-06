f0d8467f45218424441a79f07c4419d4dee64384
priyanshu10 <priyanshuparate@gmail.com>
2023-12-06 09:38:02 +0530

    c# client for spring boot service

---
+++ GrpcDotnetClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using dotnetclient;
+using Grpc.Core;
+
+
+using var channel = GrpcChannel.ForAddress("http://localhost:9000");
+var client = new StockService.StockServiceClient(channel);
+
+var stream = client.getData(new StockRequest() { Count = 10 });
+await foreach (StockPrice stok in stream.ResponseStream.ReadAllAsync<StockPrice>())
+
+
+    Console.WriteLine($"stock name: {stok.StockName} stock price : {stok.StockPrice_}");
+
+
